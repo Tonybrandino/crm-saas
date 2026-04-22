1515de900ea86cfa37007c4d5f2de8eeef57ba39
Tonybrandino <104107629+Tonybrandino@users.noreply.github.com>
2026-04-22 20:09:06 -0300

    Update firebase-app-bridge.js

--- firebase-app-bridge.js
+++ firebase-app-bridge.js
@@ -16,7 +16,8 @@ window.firebaseAuthBridge = {
   async login(email, password) {
     const cred = await signInWithEmailAndPassword(auth, email, password);
     return cred.user;
-  },
+  }
+};
 
   async register({ nome, email, password, role = "admin" }) {
     const cred = await createUserWithEmailAndPassword(auth, email, password);
