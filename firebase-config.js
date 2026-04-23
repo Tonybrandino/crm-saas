a72f2f666837cc8e536f5e55961ecff5a0ab1304
Tonybrandino <104107629+Tonybrandino@users.noreply.github.com>
2026-04-23 06:35:05 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.CRM_FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  defaultTenantId: "tenant-demo",
+  enableFirebaseAuth: false,
+  enableFirestoreSync: false
+};
